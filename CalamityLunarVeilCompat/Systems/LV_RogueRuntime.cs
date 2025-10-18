747fefe610c877359e059f7b331a028a4f643fe5
yhs6675-creator <yhs6675@gmail.com>
2025-10-19 07:37:46 +0900

    Import System namespace for Math

--- CalamityLunarVeilCompat/Systems/LV_RogueRuntime.cs
+++ CalamityLunarVeilCompat/Systems/LV_RogueRuntime.cs
@@ -1,3 +1,4 @@
+using System;
 using Microsoft.Xna.Framework;
 using Terraria;
 using Terraria.DataStructures;
@@ -40,10 +41,10 @@ namespace CLVCompat.Systems
             base.UseAnimation(item, player);
         }
 
-        public override void UseStyle(Item item, Player player)
+        public override void UseStyle(Item item, Player player, Rectangle heldItemFrame)
         {
             TryConsumeStealth(item, player);
-            base.UseStyle(item, player);
+            base.UseStyle(item, player, heldItemFrame);
         }
 
         public override bool Shoot(Item item, Player player, EntitySource_ItemUse_WithAmmo source, Vector2 position, Vector2 velocity, int type, int damage, float knockback)
